a78364b98946167921d66e6f82735f63ef785254
Nemeth Koppany Zoltan <nemeth.koppany@BOLYAI.LOCAL>
2025-05-30 09:03:34 +0200

    Fájl feltöltődik, de minden a megnevezés oszlop alá kerül

---
+++ osszegzes/functions.js
@@ -0,0 +1,76 @@
+const tablecreation = (container, callback) =>{
+    const div = document.createElement("div");
+    container.appendChild(div);
+
+    const table = document.createElement("table");
+    div.appendChild(table);
+
+    const thead = document.createElement("thead");
+    table.appendChild(thead);
+
+    const tr = document.createElement("tr");
+    thead.appendChild(tr);
+
+    const headerContent = ["Megnevezés","Hely", "Hónap", "Összeg"]
+    for(const content of headerContent){
+        const th = document.createElement("th");
+        th.innerText = content;
+        tr.appendChild(th);
+    }
+
+    const tbody = document.createElement("body");
+    table.appendChild(tbody);
+    callback(tbody);
+}
+
+const upload = (tbody, container) => {
+    const fileInput = document.createElement('input')
+    container.appendChild(fileInput);
+    fileInput.id='fileinput'
+    fileInput.type = 'file';
+
+    fileInput.addEventListener('change', (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.onload = () => {
+           const Lines = reader.result.split('\n')
+           const headerRemoved = Lines.slice(1);
+           for(const line of headerRemoved){
+                const trimmed = line.trim();
+                const fields = trimmed.split(';');
+                const alkotas = {
+                    megnevezes: fields[0],
+                    hely: fields[1],
+                    honap: fields[2],
+                    osszeg: fields[3]
+                }
+                tomb.push(alkotas);
+                rowAddition(alkotas, tbody);
+
+            }
+        }
+        reader.readAsText(file);
+    })
+}
+
+const rowAddition = (obj, tbody) =>{
+    const tr = document.createElement("tr");
+
+    const megnevezes = document.createElement("td");
+    megnevezes.textContent = obj.megnevezes;
+    tr.appendChild(megnevezes);
+
+    const hely = document.createElement("td");
+    hely.textContent = obj.hely;
+    tr.appendChild(hely);
+
+    const honap = document.createElement("td");
+    honap.textContent = obj.honap;
+    tr.appendChild(honap);
+
+    const osszeg = document.createElement("td");
+    osszeg.textContent = obj.osszeg;
+    tr.appendChild(osszeg);
+
+    tbody.appendChild(tr);
+}
